fd6597ecf8feddca251b947c3cc0f7fc87d32efe
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2023-08-27 21:00:47 -0300

    Adicionado lib `uuid` e criado função para adicionar `empregado`

--- core/index.ts
+++ core/index.ts
@@ -1,6 +1,42 @@
-function show_message(message: string): void {
-  console.log(message);
+import { v4 as uuid } from 'uuid';
+
+const employees: Array<Employee> = [
+  {
+    id: uuid(),
+    name: "funcionario padrão",
+    email: "funcionario@email.com",
+    branch: "employee"
+  }
+]
+
+const evaluations = []
+
+interface Employee {
+  id: string,
+  name: string,
+  email: string,
+  branch: string
 }
 
-show_message("teste de mensagem usando TS");
-show_message("mais uma mensagem");
+function import_employees(options: Array<Employee>): Array<Employee> {
+  const employees_merged = employees.concat(options);
+  return employees_merged;
+}
+
+const options: Array<Employee> = [
+  {
+    id: uuid(),
+    name: "Rafael Oliveira",
+    email: "rafael.oliveira@email.com",
+    branch: "dev"
+  },
+  {
+    id: uuid(),
+    name: "Alberto Cunha",
+    email: "alberto.cunha@email.com",
+    branch: "dev"
+  }
+]
+
+const employees_added = import_employees(options);
+console.log(employees_added);
